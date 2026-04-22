7d27d50a407a34cf19598e72c7ee56fcb1836669
dandmb <danbizwa@gmail.com>
2026-04-22 13:49:56 +0200

    comments usecase added

--- app/src/main/java/com/dmb25/blogpostapp/domain/usecase/comments/GetCommentsByPostUseCase.kt
+++ app/src/main/java/com/dmb25/blogpostapp/domain/usecase/comments/GetCommentsByPostUseCase.kt
@@ -1,4 +1,9 @@
 package com.dmb25.blogpostapp.domain.usecase.comments
 
-class GetCommentsByPostUseCase {
+import com.dmb25.blogpostapp.domain.repository.CommentRepository
+
+class GetCommentsByPostUseCase(
+    private val commentRepository: CommentRepository
+) {
+    operator fun invoke(postId: Int) = commentRepository.getCommentsByPost(postId)
 }
